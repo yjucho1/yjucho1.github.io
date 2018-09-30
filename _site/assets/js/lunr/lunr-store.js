44e2e7cb429581a656d23884ab53ed039e7ba2c9
yjucho <hyesung0617@naver.com>
2018-09-30 12:57:35 +0900

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
-        "title": "클러스터링을 평가하는 척도",
-        "excerpt":"클러스터링은 주어진 데이터에 대한 명시적인 정보가 많지 않을 때 유용하게 쓸수있는 머신러닝 기법 중 하나입니다. 다양한 사용자 정보를 이용해 몇가지 고객군으로 분류하여 고객군별 맞춤 전략을 도출한다던지, 유사한 상품(동영상, 음원까지도)군의 속성을 분석하여 의미있는 인사이트를 도출하는 것에 활용됩니다. 클러스터링 알고리즘 측면에서는 전통적인 Hierarchical clustering, K-means clustering 등이 비교적 쉽게 사용되고 있고, 최근에는...","categories": ["Machine Learning","Unsupervised Learning","Clustering Evaluation"],
-        "tags": [],
-        "url": "http://localhost:4000/machine%20learning/unsupervised%20learning/clustering%20evaluation/clustering-metrics/",
-        "teaser":null},{
         "title": "[번역] from GAN to WGAN",
         "excerpt":"이 글은 lilianweng의 from GAN to WGAN 포스팅을 동의하에 번역한 글입니다. From GAN to WGAN 이 포스트는 generative adversarial netowrk (GAN) model에 사용되는 수식과 GAN이 왜 학습하기 어려운지를 설명합니다. Wasserstein GAN은 두 분포간의 거리를 측정하는데 더 향상된(smoooth한) 메트릭를 사용하여 GAN의 학습과정을 개선하였습니다. Generative adversarial network(GAN)은 이미지나 자연어, 음성과 같은 현실의...","categories": ["Generative Adversarial Network","Wasserstein GAN"],
         "tags": [],
         "url": "http://localhost:4000/generative%20adversarial%20network/wasserstein%20gan/from-GAN-to-WGAN/",
+        "teaser":null},{
+        "title": "클러스터링을 평가하는 척도",
+        "excerpt":"클러스터링은 주어진 데이터에 대한 명시적인 정보가 많지 않을 때 유용하게 쓸수있는 머신러닝 기법 중 하나입니다. 다양한 사용자 정보를 이용해 몇가지 고객군으로 분류하여 고객군별 맞춤 전략을 도출한다던지, 유사한 상품(동영상, 음원까지도)군의 속성을 분석하여 의미있는 인사이트를 도출하는 것에 활용됩니다. 클러스터링 알고리즘 측면에서는 전통적인 Hierarchical clustering, K-means clustering 등이 비교적 쉽게 사용되고 있고, 최근에는...","categories": ["Machine Learning","Unsupervised Learning","Clustering Evaluation"],
+        "tags": [],
+        "url": "http://localhost:4000/machine%20learning/unsupervised%20learning/clustering%20evaluation/clustering-metrics/",
         "teaser":null}]
