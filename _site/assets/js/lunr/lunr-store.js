048563be9c6fe28aeaea1eb12bb4d4b66659f0f4
yjucho <hyesung0617@naver.com>
2018-10-12 00:13:04 +0900

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -33,4 +33,9 @@ var store = [{
         "excerpt":"개와 고양이는 어떻게 구분되는가 quick drawing은 구글에서 공개하는 오픈소스 데이터셋입니다. 345개 종류의 5백만장의 그림으로 이루어져있습니다. 이 포스팅에서는 그 중 개와 고양이 그림을 이용해 개와 고양이 그림을 구분하는 모델을 학습하고, 모델이 그림을 어떻게 인식하는지 시각화해보았습니다.import numpy as npimport matplotlib.pyplot as plt## Quick! drawing dataset## https://quickdraw.withgoogle.com/data## https://github.com/googlecreativelab/quickdraw-dataset## download : https://console.cloud.google.com/storage/browser/quickdraw_dataset/full/numpy_bitmapdogs = np.load('full%2Fnumpy_bitmap%2Fdog.npy')cats...","categories": ["Keras","Visualizing filters"],
         "tags": [],
         "url": "http://localhost:4000/keras/visualizing%20filters/quick-drawing-exec/",
+        "teaser":null},{
+        "title": "cs231n - 이해하기 2",
+        "excerpt":"cs231n http://cs231n.stanford.edu/이 포스팅은 딥러닝에 대한 기본 지식을 상세히 전달하기보다는 간략한 핵심과 실제 모델 개발에 유용한 팁을 위주로 정리하였습니다. Detection and Segmentation 1) segmeatic segmentation : sliding window Fully convolutional : labeling class per every pixel downsampling and upsampling : how to upsampling(unpooling) nearest neighbor bed of nails max unpooling(remember which...","categories": ["cs231n"],
+        "tags": [],
+        "url": "http://localhost:4000/cs231n/cs231n-2/",
         "teaser":null}]
